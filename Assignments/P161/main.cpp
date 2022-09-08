ca884badbd965aa5235e420f24ee53a1be2d34dc
Daniel Portillo <70359688+BastionWolf@users.noreply.github.com>
2022-09-08 04:16:38 -0500

    Update main.cpp

--- Assignments/P161/main.cpp
+++ Assignments/P161/main.cpp
@@ -5,6 +5,7 @@
 */
 
 #include <iostream>
+#include <iomanip>
 #include <math.h>
 
 #define endl "\n"
@@ -26,6 +27,7 @@ int main()
    // cout << "--> IN\n";
     if ((Light1 == 0) && (Light2 == 0))
     {
+      cout << '\n';
       return 0;
     }
     else if (Light2 == 0)
@@ -38,23 +40,20 @@ int main()
 
     else if (Light1 >= Light2)
     {
- //     cout << Light1 << " " << Light2 << " Top\n";
+     // cout << Light1 << " " << Light2 << " Top\n";
       Sync = (GreenSync(Light1, Light2, 0));
       Light1 = Sync;
-      
-      
     }
     else if (Light1 <= Light2)
     {
    //   cout << Light1 << " " << Light2 << " Bottom\n";
       Sync  = (GreenSync(Light2, Light1, 0));
       Light1 = Sync;
-    
     }
     else
     {cout << "Bruh";}
 
-  //  cout << "OUT -->\n";
+  
   }
   
     
@@ -63,35 +62,39 @@ int main()
 
 int GreenSync (int Num1, int Num2, int NumIter)
 {
-  int Cycle1 = (2 * Num1) + (Num1 * NumIter);
+  int Cycle1 = (2 * Num1) + (Num1 *NumIter);
   int Cycle2 = 2 *Num2;
   int Lag;
   
   Lag = Cycle1 % Cycle2;
 
-  if (Lag < (Num2 - 5))
+  if ( (NumIter == 0) && (Lag < (Num1 - 5)) && (Lag < (Num2 - 5)) )
   {
-
     return Cycle1;
   }
-  else if (Num1 >= Num2)
+  else if ( (NumIter > 0) && (Lag < (Num2 - 5)) )
   {
-    NumIter ++;
-    return GreenSync(Num1, Num2, NumIter);
+    return Cycle1;
+  }
+  else if (Cycle1 >18000)
+  {
+    return 18001;
   }
   else
   {
     NumIter ++;
-    return GreenSync(Num2, Num1, NumIter);
+    return GreenSync(Num1, Num2, NumIter);
   }
+
 }
 
 void PrintTime (int Seconds)
 {
   if (Seconds <= 18000)
   {
-    cout << (Seconds/3600) << ':' << (Seconds/60) << ':'
-      << (Seconds%60) << "  " <<'\n';
+    cout << setw(2) << setfill('0') << (Seconds/3600) << ':' 
+      << setw(2) << setfill('0') << ((Seconds%3600)/60) << ':'
+      << setw(2) << setfill('0') << (Seconds%60) << '\n';
   }
   else
   { cout << "Signals fail to synchronise in 5 hours\n";}
